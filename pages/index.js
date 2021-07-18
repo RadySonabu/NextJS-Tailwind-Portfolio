3943f8c225fb0ca640bcc9631993671ab075ffb4
RadySonabu <ardynubanos@yahoo.com>
2021-07-18 16:11:26 +0800

    Added a text in index page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ export default function index() {
 	return (
 		<div class="bg-yellow-400 h-screen">
 			<Navbar textColor="text-black" hoverColor="text-white" />
+			<div class="prose text-9xl text-center">
+				<h2>I am a software engineer.</h2>
+			</div>
 		</div>
 	);
 }
