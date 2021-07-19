e054bc853aee58f505b1bafe58de14541693e966
RadySonabu <ardynubanos@yahoo.com>
2021-07-20 01:37:20 +0800

    Added a Card content on Projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
 function projects() {
 	return (
 		<div className="bg-black h-screen">
 			<Navbar textColor="text-white" hoverColor="text-yellow-400" />
+			<div className="px-96">
+				<div className="grid grid-col-2 grid-flow-col ">
+					<Card
+						title="Backend Projects"
+						content="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quae dolores deserunt ea doloremque natus error, rerum quas
+					odio quaerat nam ex commodi hic, suscipit in a veritatis
+					pariatur minus consequuntur!"
+					/>
+					<Card
+						title="Frontend Projects"
+						content="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quae dolores deserunt ea doloremque natus error, rerum quas
+					odio quaerat nam ex commodi hic, suscipit in a veritatis
+					pariatur minus consequuntur!"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
